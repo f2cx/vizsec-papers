80ef3fbef6c90c22d6d2d8fa417e6ff3c4d9ea54
Fabian Fischer <fabian@ff.cx>
2014-11-04 14:58:25 +0100

    added README

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,5 @@
 var appServices = angular.module('appServices', ['ngResource']);
 
 appServices.factory('Paper', ['$resource', function ($resource) {
-        return $resource('papers/papers.json');
+        return $resource('papers/vizsec.json');
     }]);
